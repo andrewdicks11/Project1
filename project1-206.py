c07b54dff762cff1c1eb31a096b2ae5da9213bcc
andrewdicks11 <33812139+andrewdicks11@users.noreply.github.com>
2018-10-23 16:55:06 -0400

    Add files via upload

--- project1-206.py
+++ project1-206.py
@@ -1,5 +1,6 @@
 import os
 import filecmp
+import re
 from dateutil.relativedelta import *
 from datetime import date
 
@@ -9,15 +10,39 @@ def getData(file):
 #Input: file name
 #Ouput: return a list of dictionary objects where
 #the keys are from the first row in the data. and the values are each of the other rows
+	
+	inFile = open(file, 'r').read()
 
-	pass
+	lst1 = []
+	lines = inFile.split('\n')
+	for l in lines:
+		cells = l.split(',')
+		lst1.append(cells)
+
+	first_line = lst1[0]
+	lst2 = lst1[1:]
+	lst_of_dict = []
+	for x in lst2:
+		d = {}
+		for idx in range(len(x)):
+			d[first_line[idx]] = x[idx]
+		lst_of_dict.append(d)
+
+	return lst_of_dict
+
 
 def mySort(data,col):
 # Sort based on key/column
 #Input: list of dictionaries and col (key) to sort on
 #Output: Return the first item in the sorted list as a string of just: firstName lastName
+	
+	sorted_data = sorted(data, key=lambda x: x[col])
 
-	pass
+	firstName = sorted_data[0]['First']
+	lastName = sorted_data[0]['Last']
+	output = firstName + " " + lastName
+	
+	return output
 
 
 def classSizes(data):
@@ -26,8 +51,25 @@ def classSizes(data):
 # Output: Return a list of tuples sorted by the number of students in that class in
 # descending order
 # [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
+	
+	fresh = 0
+	soph = 0
+	jun = 0
+	sen = 0
+	for student in data:
+		if student['Class'] == 'Freshman':
+			fresh += 1
+		elif student['Class'] == 'Sophomore':
+			soph += 1
+		elif student['Class'] == 'Junior':
+			jun += 1
+		else:
+			sen += 1
 
-	pass
+	classes_dict = {'Freshman':fresh, 'Sophomore':soph, 'Junior':jun, 'Senior':sen}
+	sorted_classes = sorted(classes_dict.items(), key=lambda x: x[1], reverse=True)
+
+	return sorted_classes
 
 
 def findMonth(a):
@@ -35,7 +77,25 @@ def findMonth(a):
 # Input: list of dictionaries
 # Output: Return the month (1-12) that had the most births in the data
 
-	pass
+	dates = []
+	for student in a:
+		dates.append(student['DOB'])
+
+	months = []
+	for date in dates:
+		x = re.findall('(\d{1,2})/\d{1,2}/\d{4}', date)
+		months += x
+
+	freq = {}
+	for month in months:
+		if month not in freq:
+			freq[month] = 0
+		freq[month] += 1
+
+	sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+	return int(sorted_freq[0][0])
+
 
 def mySortPrint(a,col,fileName):
 #Similar to mySort, but instead of returning single
@@ -44,7 +104,18 @@ def mySortPrint(a,col,fileName):
 #Input: list of dictionaries, col (key) to sort by and output file name
 #Output: No return value, but the file is written
 
-	pass
+	outFile = open(fileName, 'w')
+
+	sorted_data = sorted(a, key=lambda x: x[col])
+
+	for student in sorted_data:
+		first = student['First']
+		last = student['Last']
+		email = student['Email']
+		outFile.write('{},{},{}\n'.format(first, last, email))
+
+	outFile.close()
+
 
 def findAge(a):
 # def findAge(a):
@@ -53,7 +124,29 @@ def findAge(a):
 # integer.  You will need to work with the DOB and the current date to find the current
 # age in years.
 
-	pass
+	today = date.today()
+	current_year = today.year
+	current_month = today.month
+	current_day = today.day
+
+	total_age = 0
+	for student in a:
+		dob = student['DOB']
+		year = re.findall('\d{1,2}/\d{1,2}/(\d{1,4})', dob)
+		month = re.findall('(\d{1,2})/\d{1,2}/\d{1,4}', dob)
+		day = re.findall('\d{1,2}/(\d{1,2})/\d{1,4}', dob)
+
+		date_of_birth = date(int(year[0]), int(month[0]), int(day[0]))
+
+		age = abs(today - date_of_birth)
+		age_in_days = int((str(age)).split()[0])
+		age_in_years = age_in_days / 365
+
+		total_age += age_in_years
+
+	avg_age = total_age / len(a)
+
+	return int(avg_age)
 
 
 ################################################################
